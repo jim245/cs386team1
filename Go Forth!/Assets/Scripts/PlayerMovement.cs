da86c7b651bfe60e8e44ec5907540ebe195e25b1
AngusA1210 <aja662@nau.edu>
2023-10-20 14:44:14 -0700

    Adds horizontal movement to knight

--- Go Forth!/Assets/Scripts/PlayerMovement.cs
+++ Go Forth!/Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,26 @@ public class PlayerMovement : MonoBehaviour
 {
     // Variables
     public Rigidbody2D playerRigidBody;
+    public float speed;
+    public float input;
 
     // Start is called before the first frame update
-    void Start()
+   /* void Start()
     {
         
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Acquires input
+        input = Input.GetAxisRaw("Horizontal");
+    }
+
+    // Set to run 50 times per second to allow steady movement
+    void FixedUpdate()
+    {
+        // Moves player on the x-axis
+        playerRigidBody.velocity = new Vector2(input * speed, playerRigidBody.velocity.y);
     }
 }
