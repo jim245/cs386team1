136ca62dcad73611ea64330342d8bc82ad5ad60f
clm693 <93413915+clm693@users.noreply.github.com>
2023-11-18 23:52:01 -0700

    Score text part 1000
    Old branch only conflicted. This should work

--- Go Forth!/Assets/Scripts/ScoreHolder.cs
+++ Go Forth!/Assets/Scripts/ScoreHolder.cs
@@ -1,22 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class ScoreHolder : MonoBehaviour
 {
     // TMP_Text scoreUI_Text;
     public int scoreNum = 0;
-    //scoreUI_Text.text = scoreNum.ToString();
-
-    void Start()
-    {
-        //scoreUI_Text.text = scoreNum.ToString();
-    }
+    public Text scoreText;
 
     public void changeScore(float time)
     {
         scoreNum += (int)((time*time)/5);
-        //scoreUI_Text.text = scoreNum.ToString();
+        scoreText.text = scoreNum.ToString();
     }
 }
