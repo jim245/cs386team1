c2af9afe00452f819d0a5ac7cd50e09db87fcf3e
AngusA1210 <aja662@nau.edu>
2023-11-19 15:00:12 -0700

    Adds functionality to checkpoint flag

---
+++ Go Forth!/Assets/Scripts/CheckPointScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Updates respawn point
+            GameObject.Find("RespawnPoint").transform.position = GameObject.Find("CheckpointFlag").transform.position;
+        }
+    }
+}
