37ff0462e45adfbcac690f15f005050fe38cd9e5
AngusA1210 <aja662@nau.edu>
2023-11-19 15:10:34 -0700

    Fixes naming of CheckpointScript

--- Go Forth!/Assets/Scripts/CheckPointScript.cs
+++ Go Forth!/Assets/Scripts/CheckPointScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckPointScript : MonoBehaviour
+public class CheckpointScript : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
